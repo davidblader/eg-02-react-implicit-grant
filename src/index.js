b2c1a4f59412725841f09b18647595fc914b1ecf
davidblader <dblader@getquilt.com>
2019-05-28 16:22:10 -0400

    rm console.log

--- src/index.js
+++ src/index.js
@@ -15,7 +15,6 @@ import loadingReducer from './store/reducers/loading';
 import sendSignEnvReducer from './store/reducers/sendSignEnv';
 
 const composeEnhancers = (process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null) || compose;
-console.log(window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__);
 const rootReducer = combineReducers({
     dsAuth: dsAuthReducer,
     listEnv: listEnvReducer,
