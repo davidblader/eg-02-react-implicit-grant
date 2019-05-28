e3437f87ffbc0864eb1686fc02f4791d38a922d6
davidblader <dblader@getquilt.com>
2019-05-28 16:18:43 -0400

    wrap ternary in parentheses

--- src/index.js
+++ src/index.js
@@ -14,8 +14,8 @@ import listEnvReducer from './store/reducers/listEnv';
 import loadingReducer from './store/reducers/loading';
 import sendSignEnvReducer from './store/reducers/sendSignEnv';
 
-const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
-
+const composeEnhancers = (process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null) || compose;
+console.log(window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__);
 const rootReducer = combineReducers({
     dsAuth: dsAuthReducer,
     listEnv: listEnvReducer,
